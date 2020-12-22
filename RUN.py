1982f407426e499cbf5cfc83b2702e41417e0138
EC2 Default User <ec2-user@ip-172-31-34-141.us-east-2.compute.internal>
2020-12-22 23:14:10 +0900

    aaa

---
+++ RUN.py
@@ -0,0 +1,18 @@
+# import RSI_LOGIC
+# import GET_PERSONAL_INFO
+# import GET_PRICE
+
+
+
+
+import yaml
+with open('FIRE_BOT/config.yaml', 'r') as yml:
+    config = yaml.load(yml)
+ 
+print('asa: ', config['asa']['aisatsu'])
+print('hiru: ', config['hiru']['aisatsu'])
+print('yoru: ', config['yoru']['aisatsu'])
+a=time
+import a
+
+config['set']['imp']
